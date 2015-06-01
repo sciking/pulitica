d054fa858f7c2b1e036ed67a0e676a870da65834
sciking <sciking@users.noreply.github.com>
2015-06-02 00:37:30 +0200

    Update gioco.py

--- gioco.py
+++ gioco.py
@@ -9,12 +9,13 @@ import random
 turno = 1
 elettori = 15
 flop = 0
+r = 1
 sindaci = 45
 ele1 = 50
 deputati = 15
 ele2 = 40
 eleap = 5
-leggi = ["contro l'omofobia", "per l'introduzione del lombardo nelle scuole", "contro la ciarlataneria", "per la democrazia diretta", "contro l'obbligo militare", "per l'allungamento dell'obbligo scolastico", "contro le pantofole", "per la sicurezza scolastica", "per l'abolizione del reato di stupro", "per l'illegalità del comunismo"]
+leggi = ["contro l'omofobia", "per l'introduzione del lombardo nelle scuole", "contro la ciarlataneria", "per la democrazia diretta", "contro l'obbligo militare", "per l'allungamento dell'obbligo scolastico", "contro le pantofole", "per la sicurezza scolastica", "per l'abolizione del reato di stupro", "per l'illegalità del popolarismo", "per i diritti civili","per abolire le religioni", "per l'annessione dello Stato Emiliano", "per l'istituzione delle regioni", "per abolire i videogiochi violenti","per l'elezione diretta del Premier","per la prevenzione dell'obesità","per l'introduzione del matrimionio incestuoso","per l'ufficializzazione della lingua lombarda","per ridurre l'inquinamento","per favorire le lobby del tabacco","per costruire una statua della Perottina a Pregnana","per aumentare le ore di educazione sessuale","per punire la bestemia","per abolire le scarpe col tacco","per diminuire le tasse","per aumentare le pene per omicidio stradale","contro il nomadismo","contro il software proprietario","contro la pirateria informatica","per le adozioni ai single e ai gay","Per regolare l'immigrazione dall'Est Europa","Per l'introduzione dei Permessi d'Accesso","per introdurre le console da videogioco a scuola","contro il gimnopodismo","per l'eliminazione delle zanzare","per fornire ai cittadini buoni gratis per il postribolo.","per proibire l'alcole","per aumentare i finanziamenti alle scuole","per abolire le scuole private"]
 parlamento = ele1 + ele2 + eleap + elettori
 # deputati = 50
 print """ Benvenuto. L'assemblea del Partito Indipendente Milanese ti ha nominato Presidente. Siamo un piccolo partito con un peso minimo nella politica milanese, ma ti abbiamo scelto per far crescere il nostro partito e renderlo uno dei primi. Il nostro obiettivo è di eleggere il Capo dello Stato"""
@@ -23,15 +24,28 @@ nome = raw_input("Come ti chiami, Presidente?: ")
 raw_input("Premi invio per iniziare:")
 os.system("clear")
 def poss():
-	print "L'Indipendente di Milano"
 	global elettori
 	global flop
 	global ele1
 	global deputati
+	global r
 	global ele2
+	global leggi
 	global parlamento
 	global eleap
 	global sindaci
+	opzioni = ["si", "no", "si", "no", "no"]
+	j = random.randint(0,4)
+	opz = opzioni[j]
+	if opz == "si":
+		print "Legge approvata! \n \n"
+		elettori = elettori + 1
+		del leggi[r] #decommentare quando ci saranno molte leggi
+	else:
+		print "Legge respinta\n \n"
+		elettori = elettori - 1 
+	
+	print "L'Indipendente di Milano"
 	#global deputati
 	poss = random.randint(1,20)
 	if poss == 1:
@@ -147,13 +161,12 @@ def gioco():
 	global ele2
 	global eleap
 	global sindaci
+	global r
 	global nome
 	global flop
 	global parlamento
 	global deputati
-	print ""
-	print ""
-	print ""
+	print " \n \n \n "
 	if turno%5 == 0:
 		deputati = elettori
 		if parlamento/deputati < 2 and flop < 2:
@@ -189,24 +202,15 @@ Lei non ci serve più come presidente. Siamo riusciti ad eleggere il Capo dello 
 		ele1 = ele1 + random.randint(-3,3)
 		ele2 = ele2 + random.randint(-3,3)
 		eleap = eleap + random.randint(-3,3)
-	opzioni = ["si", "no", "si", "no"]
-	j = random.randint(0,3)
-	opz = opzioni[j]
-	if opz == "si":
-		print "Legge approvata!"
-		elettori = elettori + 1
-		#del legge[r]
-	else:
-		print "Legge respinta"
-		elettori = elettori - 1
 
 	turno = turno +1 
 	if sindaci > 240:
-		print "Grazie Presidente, vogliamo mandarti in campo! Hai vinto, possediamo quasi tutti i comuni ora. Abbiamo eletto il Capo Dello Stato con maggioranza dei comuni e sei tu l'attuale Capo dello Stato. Governa bene!"
+		print "Grazie Presidente, vogliamo mandarti in campo! Hai vinto, possediamo quasi tutti i comuni ora. Abbiamo eletto il Capo Dello Stato con maggioranza dei comuni e sei tu l'attuale Capo dello Stato. Governa bene! \n Onori al Capo dello Stato", nome, "!"
 		exit()
 		
 	if flop > 5:
-		print "Hai sbagliato troppe volte, mi spiace. Sei licenziato"
+		print "Hai sbagliato troppe volte, mi spiace. Sei licenziato \n"
+		print "L'Indipendente di Milano \n", nome, "fallisce, ma sarà Sindaco"
 		exit()
 	os.system("clear")
 	poss()
