949354e00c69fb43eec03ee514a8c62facb692a4
Mike Sciking <sciking@users.noreply.github.com>
2015-06-02 06:50:48 +0200

    Enormi modifiche

--- gioco.py
+++ gioco.py
@@ -12,15 +12,19 @@ flop = 0
 r = 1
 sindaci = 45
 ele1 = 50
-deputati = 15
 ele2 = 40
 eleap = 5
 pa = ["Partito Liberale", "Lega Milanese", "Unione Popolare", "Movimento delle Libertà", "Casa Riformista", "Il Lume della Ragione"]
 pb = ["Milano Rossa", "Partito Ecologista", "Lega dei Contadini", "Partito per le frontiere aperte", "Progressismo è Democrazia", "Rivoluzione Popolare"]
 destra = pa[random.randint(0,5)]
 sinistra =  pb[random.randint(0,5)]
-leggi = ["contro l'omofobia", "per l'introduzione del lombardo nelle scuole", "contro la ciarlataneria", "per la democrazia diretta", "contro l'obbligo militare", "per l'allungamento dell'obbligo scolastico","per creare un testo all'Inno Nazionale","per proibire la Lingua Inglese","per la beatificazione di Freddie Mercury","per aumentare le pensioni","per abolire la canapa a Milano", "contro le pantofole", "per la sicurezza scolastica", "per l'abolizione del reato di stupro", "per l'illegalità del popolarismo", "per i diritti civili","per abolire le religioni", "per l'annessione dello Stato Emiliano", "per l'istituzione delle regioni", "per abolire i videogiochi violenti","per l'elezione diretta del Premier","per la prevenzione dell'obesità","per l'introduzione del matrimionio incestuoso","per l'ufficializzazione della lingua lombarda","per ridurre l'inquinamento","per favorire le lobby del tabacco","per costruire una statua della Perottina a Pregnana","per aumentare le ore di educazione sessuale","per punire la bestemia","per abolire le scarpe col tacco","per diminuire le tasse","per aumentare le pene per omicidio stradale","contro il nomadismo","per le pari opportunità","per lo ius soli","contro le droghe","contro il software proprietario","contro la pirateria informatica","per le adozioni ai single e ai gay","Per regolare l'immigrazione dall'Est Europa","Per l'introduzione dei Permessi d'Accesso","per introdurre le console da videogioco a scuola","contro il gimnopodismo","per l'eliminazione delle zanzare","per fornire ai cittadini buoni gratis per il postribolo.","per proibire l'alcole","per aumentare i finanziamenti alle scuole","per abolire le scuole private", "contro le sculacciate",]
-parlamento = ele1 + ele2 + eleap + elettori
+leggi = ["contro l'omofobia", "per l'introduzione del lombardo nelle scuole", "contro la ciarlataneria", "per la democrazia diretta", "contro l'obbligo militare", "per l'allungamento dell'obbligo scolastico","per creare un testo all'Inno Nazionale","per proibire la Lingua Inglese","per la beatificazione di Freddie Mercury","per aumentare le pensioni","per abolire la canapa a Milano", "contro le pantofole", "per la sicurezza scolastica", "per l'abolizione del reato di stupro","per l'insegnamento del russo", "per l'illegalità del popolarismo", "per i diritti civili","per abolire le religioni", "per l'annessione dello Stato Emiliano", "per l'istituzione delle regioni", "per abolire i videogiochi violenti","per l'elezione diretta del Premier","per la prevenzione dell'obesità","per l'introduzione del matrimionio incestuoso","per l'ufficializzazione della lingua lombarda","per ridurre l'inquinamento","per favorire le lobby del tabacco","per costruire una statua della Perottina a Pregnana","per aumentare le ore di educazione sessuale","per punire la bestemia","per abolire le scarpe col tacco","per diminuire le tasse","per aumentare le pene per omicidio stradale","contro il nomadismo","per le pari opportunità","per lo ius soli","contro le droghe","contro il software proprietario","contro la pirateria informatica","per le adozioni ai single e ai gay","Per regolare l'immigrazione dall'Est Europa","Per l'introduzione dei Permessi d'Accesso","per introdurre le console da videogioco a scuola","contro il gimnopodismo","per l'eliminazione delle zanzare","per fornire ai cittadini buoni gratis per il postribolo.","per proibire l'alcole","per aumentare i finanziamenti alle scuole","per abolire le scuole private", "contro le sculacciate",]
+emergenza = tuple(leggi)
+parlamento = 220
+deputati = elettori*parlamento/(elettori+ele1+ele2+eleap)# elettori : totale = x : parlamento
+depd = ele1*parlamento/(elettori+ele1+ele2+eleap)
+deps = ele2*parlamento/(elettori+ele1+ele2+eleap)
+depa = eleap*parlamento/(elettori+ele1+ele2+eleap)
 # deputati = 50
 print """ Benvenuto. L'assemblea del Partito Indipendente Milanese ti ha nominato Presidente. Siamo un piccolo partito con un peso minimo nella politica milanese, ma ti abbiamo scelto per far crescere il nostro partito e renderlo uno dei primi. Il nostro obiettivo è di eleggere il Capo dello Stato"""
 print " Gioco creato da Sciking"
@@ -71,9 +75,11 @@ def poss():
 			sindaci = sindaci + 20
 			gioco()
 	elif poss == 4:
-		print "Rinnovo straordinario del nostro Parlamento"
-		deputati = elettori
-		print elettori, "nostri deputati al Parlamento su", parlamento
+		print "Scandalo Scommesse nel Calcio"
+		print destra,"e Partito Indipendente non ne sono dentro"
+		elettori = elettori + 3	
+		ele1 = ele1 + 3
+		ele2 = ele2 - 5
 		gioco()
 	elif poss == 5:
 		print "Voto nella Dichiarazione di Guerra: Il tuo partito è favorevole, il popolo apprezza"
@@ -105,7 +111,7 @@ def poss():
 		gioco()
 	elif poss == 10:
 		print "La prostituzione legale è legge, grazie a noi"
-		elettori = elettori + 6
+		elettori = elettori + 6 #prima era 8... suvvia, non siamo così puttanieri :D 
 		gioco()
 	elif poss == 11:
 		print "I giudici riescono a condannare l'imputato della strage di 3 anni fa: Ergastolo!"
@@ -167,17 +173,26 @@ def gioco():
 	global eleap
 	global sindaci
 	global r
+	global emergenza
 	global nome
 	global flop
+	global deps
+	global depa
+	global depd
+	global sinistra
+	global destra
 	global parlamento
+	global leggi
 	global deputati
 	print " \n \n \n "
 	if turno%5 == 0:
-		deputati = elettori
+		deputati = elettori*parlamento/(elettori+ele1+ele2+eleap)# elettori : totale = x : parlamento
+		depd = ele1*parlamento/(elettori+ele1+ele2+eleap)
+		deps = ele2*parlamento/(elettori+ele1+ele2+eleap)
+		depa = eleap*parlamento/(elettori+ele1+ele2+eleap)
 		if parlamento/deputati < 2 and flop < 2:
 			print """Salve signor presidente. Siamo riusciti ad eleggere il Capo dello Stato e abbiamo scelto Lei come nostro nuovo Capo. Governi bene in questi due anni!"""
 			print "Onori al Capo dello Stato", nome, "!"
-			gioco()
 			#capostato()
 		elif parlamento/deputati < 2:
 			print "Salve, Presidente. Abbiamo eletto il nostro nuovo presidente. Aumenterà i nostri elettori."
@@ -189,14 +204,20 @@ def gioco():
 		elettori = elettori + 3
 	print "Turno", turno
 	print "hai", elettori, "elettori"
-	print "Parlamentari", deputati, "/", parlamento
+	print "Parlamentari Partito Indipendente:", deputati, "/", parlamento
+	print "Parlamentari", destra, ":" ,depd, "/", parlamento
+	print "Parlamentari", sinistra, ":",deps, "/", parlamento
+	print "Parlamentari misti:", depa, "/", parlamento
 	print "Sindaci", sindaci, "/250"
 	print "Altri:"
 	print "Elettori", destra, ":", ele1
 	print "Elettori", sinistra, ":", ele2
 	print "LEGGE DEL GIORNO:"
-	r = random.randint(1,9)
-	print "legge", leggi[r]
+	try:
+		r = random.randint(1,9)
+		print "legge", leggi[r]
+	except:
+		leggi = list(emergenza)
 	scelta = raw_input("Vuoi approvare la legge?")
 	scelta = scelta.lower
 	if scelta == "si":
@@ -215,10 +236,20 @@ def gioco():
 		print "Grazie Presidente, vogliamo mandarti in campo! Hai vinto, possediamo quasi tutti i comuni ora. Abbiamo eletto il Capo Dello Stato con maggioranza dei comuni e sei tu l'attuale Capo dello Stato. Governa bene! \n Onori al Capo dello Stato", nome, "!"
 		exit()
 		
-	if flop > 5:
+	if flop > 5 and livello > 25:
 		print "Hai sbagliato troppe volte, mi spiace. Sei licenziato \n"
 		print "L'Indipendente di Milano \n", nome, "fallisce, ma sarà Sindaco"
 		exit()
+	if elettori < 0:
+		elettori = 1
+		flop = flop + 1
+	if ele1 < 5:
+		destra = pa[random.randint(0,5)]
+		ele1 = 15
+	if ele2 < 5:
+		sinistra =  pb[random.randint(0,5)]
+		ele2 = 15
+		
 	os.system("clear")
-	poss()
+	poss() 
 gioco()		# computerblog.ga no alle pantofole :D
